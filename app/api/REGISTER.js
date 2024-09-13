4ed21c93c5a91a72cc41ef8fbff6bc6970f2df4d
Shumais Qasim <rrichie551@gmail.com>
2024-09-13 16:46:31 +0530

    Create REGISTER.js

---
+++ app/api/REGISTER.js
@@ -0,0 +1,11 @@
+export const REGISTER = `mutation{
+    cartTransformCreate(functionId: "5f98bf64-48f1-4bdd-8c89-7abd5f1984fc"){
+      cartTransform{
+        id
+      }
+      userErrors{
+        message
+      }
+    }   
+  }
+`;
